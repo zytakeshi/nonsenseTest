e123f12b88ff0c6f9ca8b1e831e2ab1aaa48a777
zytakeshi <zyvista@gmail.com>
2020-05-17 15:39:19 +0900

    Update v2.sh

--- v2.sh
+++ v2.sh
@@ -1,6 +1,10 @@
 sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
 systemctl stop firewalld systemctl disable firewalld systemctl status firewalld
 sudo setenforce 0
+sed -i '/^\*\ *soft\ *nofile\ *[[:digit:]]*/d' /etc/security/limits.conf
+sed -i '/^\*\ *hard\ *nofile\ *[[:digit:]]*/d' /etc/security/limits.conf
+echo '* soft nofile 65536' >>/etc/security/limits.conf
+echo '* hard nofile 65536' >>/etc/security/limits.conf
 sudo yum remove firewalld ufw -y
 yum install epel-release -y
 yum --enablerepo=epel update -y
