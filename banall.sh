cdc5e95ad10a0c0725cfd5a8270d47b17c5efb59
zytakeshi <zyvista@gmail.com>
2020-05-20 12:21:52 +0900

    Create banall.sh

---
+++ banall.sh
@@ -0,0 +1,3 @@
+wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubiBackup/doubi/master/ban_iptables.sh && chmod +x ban_iptables.sh && bash ban_iptables.sh
+./ban_iptables.sh banall
+echo 'done'
