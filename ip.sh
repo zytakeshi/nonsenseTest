9ec7f32a3907f3bc513bfc7eeeef51a84f0f1541
zytakeshi <zyvista@gmail.com>
2020-05-21 15:46:39 +0900

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -1,2 +1,7 @@
-iptables -F
+iptables -I INPUT -s	0.0.0.0	 -p tcp --dport 26580 -j ACCEPT
+iptables -I INPUT -s	127.0.0.1	 -p tcp --dport 26580 -j ACCEPT
+iptables -I INPUT -s   42.157.160.112  -p tcp --dport 26580 -j ACCEPT
+iptables -I INPUT -s	45.155.225.252 -p tcp --dport 26580 -j ACCEPT
+iptables -A INPUT -p tcp --dport 26580 -j DROP
+iptables -A INPUT -p tcp --dport 26580 -j DROP
 echo 'OK'
