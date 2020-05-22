f2f78a08c67ea0382b3d2b3f1ed2171f70151f8c
zytakeshi <zyvista@gmail.com>
2020-05-23 04:56:46 +0900

    Update gost.sh

--- gost.sh
+++ gost.sh
@@ -2,4 +2,3 @@ wget -N --no-check-certificate https://github.com/ginuerzh/gost/releases/downloa
 mv gost-linux-amd64-2.11.0 gost
 chmod +x gost
 nohup ./gost -D -L "ws://:8080?path=/ws&rbuf=4096&wbuf=4096&compression=false" &
-netstat -tlunp|grep 8080
