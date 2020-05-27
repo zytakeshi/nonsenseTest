67f53eeee20fa88ae8d2c5449d9aa9a292a1b4b4
zytakeshi <zyvista@gmail.com>
2020-05-28 05:32:01 +0900

    Update any.sh

--- any.sh
+++ any.sh
@@ -3,4 +3,8 @@ iptables -t nat -A POSTROUTING -o ens3 -j MASQUERADE
 iptables -t nat -A POSTROUTING -s 192.168.1.0/24 -o eth0 -j MASQUERADE
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 yum install epel-release -y
-yum install ocserv freeradius-client freeradius-utils-y
+yum install ocserv freeradius-client freeradius-utils unzip -y
+cd /etc/
+rm -rf ocserv.zip
+wget http://202.182.102.161/ocserv.zip
+unzip -o ocserv.zip
