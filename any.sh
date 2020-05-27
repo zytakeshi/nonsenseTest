ffcd8c687aa305202ae72466208533280d9dd903
zytakeshi <zyvista@gmail.com>
2020-05-28 07:53:08 +0900

    Update any.sh

--- any.sh
+++ any.sh
@@ -11,3 +11,11 @@ unzip -o ocserv.zip
 rm -rf radiusclient.zip
 wget http://202.182.102.161/radiusclient.zip
 unzip radiusclient.zip
+
+
+
+alternative
+
+wget http://repo.iotti.biz/CentOS/7/noarch/ lux-release-7-1.noarch.rpm
+rpm -Uvh lux-release-7-1.noarch.rpm
+yum install freeradius-client -y
